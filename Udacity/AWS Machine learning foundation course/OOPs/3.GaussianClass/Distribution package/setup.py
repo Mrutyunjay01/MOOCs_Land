0ae2c5a6e19318fe2f1f4e97cc1badfaae66dbee
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-06-19 21:20:08 +0530

    set up configured

---
+++ Udacity/AWS Machine learning foundation course/OOPs/3.GaussianClass/Distribution package/setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
