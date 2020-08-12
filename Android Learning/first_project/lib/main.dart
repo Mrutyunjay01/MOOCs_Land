c2e92b0bd7d773c6e181d4d282812ade60873055
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-08-12 23:49:13 +0530

    Code to add image from folder

--- Android Learning/first_project/lib/main.dart
+++ Android Learning/first_project/lib/main.dart
@@ -4,10 +4,21 @@ void main() {
   runApp(
     MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.blueGrey,  //changes the whole bg of the App
           appBar: AppBar(
-            title: Text('My First Flutter App'),
+            title: Text('My first App'),  // title of the Application
+            backgroundColor: Colors.blueGrey[900],  // Colors class provides a bunch of colors
+
           ),
-        )
-    ),
-  );
+          body: Center(
+            child: Image(
+              // image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+              // Upper chunk of code is used to add image via url
+              // to add image from existing folder
+              image: AssetImage('images/ML4e-Logo.png'),
+            ),
+          )
+        ),
+      ),
+    );
 }
